13356e68186cadbe93cc1482262225b248b8367d
Forcxy <2893008306@qq.com>
2023-01-07 15:27:08 +0800

    完成了首页开发

---
+++ subpkg/goods_list/goods_list.vue
@@ -0,0 +1,19 @@
+<template>
+  <view>
+    Good-list
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      };
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
